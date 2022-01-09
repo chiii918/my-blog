457331fbf4c131f7dad293401ae130af3731f154
土橋千尋 <58905313+chiii918@users.noreply.github.com>
2022-01-09 22:06:20 +0900

    change: Adobefontsの読み込み

--- src/components/seo.js
+++ src/components/seo.js
@@ -71,12 +71,28 @@ const Seo = ({ description, lang, meta, title }) => {
           content: metaDescription,
         },
       ].concat(meta)}
-    />
+    >
+      {/* webfont */}
+      <script type="application/javascript">
+        {`
+          {
+            (function(d) {
+              var config = {
+                kitId: 'aam2xpx',
+                scriptTimeout: 3000,
+                async: true
+              },
+              h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+            })(document);
+          }
+        `}
+      </script>
+    </Helmet>
   )
 }
 
 Seo.defaultProps = {
-  lang: `en`,
+  lang: `ja`,
   meta: [],
   description: ``,
 }
