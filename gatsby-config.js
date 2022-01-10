b83137116f3b2283c8c9909ca92a8fcfe46da388
土橋千尋 <58905313+chiii918@users.noreply.github.com>
2022-01-10 16:56:41 +0900

    fix: gatsby-plugin-google-analyticsからgatsby-plugin-google-gtagに変更
    新しいバージョンのGAはgtagを使うため変更

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,12 +51,21 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     trackingId: `ADD YOUR TRACKING ID HERE`,
-    //   },
-    // },
+    {
+      // google サチコン、アナリティクス、アドセンス登録
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          "G-5K7B0W2L1H", // Google Analytics / GA
+          //"AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        pluginConfig: {
+          // <head>内にタグを記述するかどうか
+          head: true,
+        },
+      },
+    },
     {
       resolve: `gatsby-plugin-feed`,
       options: {
